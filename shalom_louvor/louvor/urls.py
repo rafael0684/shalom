39dae0d3c3276b264b701b8e7ab3ad8ee1ff4088
Rafael Almeida <rafael0684@hotmail.com>
2022-08-28 21:37:13 +0100

    Ajustes de busca por musica/letras

--- shalom_louvor/louvor/urls.py
+++ shalom_louvor/louvor/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('songs', views.songs, name='songs' ),
+    path('buscar', views.buscar, name='buscar' ),
     path('<int:musica_id>', views.musica, name='musica'),
     path('musica/<int:musica_id', views.musica)
     
--- shalom_louvor/louvor/views.py
+++ shalom_louvor/louvor/views.py
@@ -32,4 +32,17 @@ def musica(request, musica_id):
     return render(request, 'musica.html', musica_a_exibir )
 
 def buscar(request):
-    return render(request, 'buscar.html')
+    
+    lista_musicas = Musica.objects.order_by('-data_musica')
+    
+    if 'buscar' in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if buscar:
+            lista_de_musicas = lista_musicas.filter(titulo_musica__icontains=nome_a_buscar)
+            lista_lyrics = lista_musicas.filter(lyrics__icontains=nome_a_buscar)
+            dados = {
+                'musicas' : lista_de_musicas,
+                'musicas' : lista_lyrics,
+            }
+
+    return render(request, 'buscar.html', dados)
