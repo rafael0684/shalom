522d6b198c51fb2ab7feb29ff9fccb0dcc6d8aa0
Rafael Almeida <rafael0684@hotmail.com>
2022-09-04 02:06:36 +0100

    Templates para login e cadastro funcionando

--- shalom_louvor/usuarios/views.py
+++ shalom_louvor/usuarios/views.py
@@ -1,55 +1,72 @@
-from cmd import PROMPT
-import email
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import auth
+from django.contrib.auth import authenticate
+from louvor.models import Musica, Artista
 
-# Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        email =request.POST['email']
+        senha = request.POST['senha']
+        
+        if email.strip() == '' or senha.strip() == '':
+            print('Os campos email e senha nao podem ficar em branco')
+            return redirect('musica')
+        else:
+            print(email, senha)
+            if User.objects.filter(email=email).exists():
+                
+                nome = User.objects.filter(email=email).values_list('username', flat=True).get()
+                user = auth.authenticate(request, username=nome, password=senha)
+                if user is not None:
+                    auth.login(request, user)
+                    print('Login realizado com sucesso')   
+                    return redirect('dashboard')
+                else:
+                    print('deu ruim o user eh None')
+            else:
+                print('email nao existe usuario com esse email')
+        return render(request,'musica.html')
+    
+    
+
 def cadastro(request):
     if request.method == 'POST':
-        nome = request.POST['nome']
+        
+        nome = request.POST['nome'].capitalize()
+        sobrenome = request.POST['sobrenome'].capitalize()
         email = request.POST['email']
         senha = request.POST['password']
         senha2 = request.POST['password2']
+        username= nome.lower()+sobrenome.lower()
 
         if not nome.strip():
             print("O campo nome nao pode ficar em branco")
-            return redirect('cadastro')
+            return redirect('index')
         if senha != senha2:
             print('As senhas nao sao iguais')
             return redirect('index')
         if User.objects.filter(email = email).exists():
             print('Usuario ja cadastrado')
-            return redirect('cadastro')
-        user = User.objects.create_user(username=nome, email=email, password=senha)
+            return redirect('index')
+        user = User.objects.create_user(username=username, first_name=nome, last_name=sobrenome, email=email, password=senha)
         user.save()
         print('Usuario cadastrado com sucesso')
-        return redirect('index')
+        user = auth.authenticate(request, username=username, password=senha)
+        auth.login(request, user)
+        return redirect('dashboard')
         
     else:
         return render(request, 'cadastro.html')
 
-def login(request):
-    if request.method == 'POST':
-        nome =request.POST['email']
-        senha = request.POST['password']
-        
-        if nome.strip() == '' or senha.strip() == '':
-            print('Os campos email e senha nao podem ficar em branco')
-            return redirect('login')
-        else:
-            print(nome, senha)
-            if User.objects.filter(email=nome).exists():
-                nome = User.objects.filter(email=nome).values_list('username', flat=True).get
-                print(nome)
-                
-            return redirect('dashboard')
-        
-    return render(request,'index.html')
-    
-
-
 def logout(request):
-    return render(request, 'logout.html')
+    auth.logout(request)
+    return redirect('index')
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    musicas = Musica.objects.order_by('-data_musica').all
+    dados = {
+        'musicas' : musicas
+    }
+    return render(request, 'dashboard.html', dados)
